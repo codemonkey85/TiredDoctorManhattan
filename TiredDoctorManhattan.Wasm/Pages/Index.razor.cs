f14f2330d675754b23e2de079df4a7ad1d2bd45b
Michael Bond <michael@bondcodes.com>
2023-02-15 02:19:16 -0500

    Accessibility improvements

--- TiredDoctorManhattan.Wasm/Pages/Index.razor.cs
+++ TiredDoctorManhattan.Wasm/Pages/Index.razor.cs
@@ -6,13 +6,26 @@ public partial class Index
     private string TextToRender { get; set; } = default!;
     private byte[]? ImageBytes { get; set; }
     private string? GetImageBase64 => ImageBytes is null ? null : Convert.ToBase64String(ImageBytes);
+    private bool IsWorking { get; set; } = false;
 
     private async Task GenerateImage()
     {
-        var text = TiredManhattanGenerator.Clean(TextToRender);
-        using var backgroundStream = await HttpClient.GetStreamAsync("assets/background.png");
-        using var fontStream = await HttpClient.GetStreamAsync("assets/KMKDSPK_.ttf");
-        ImageBytes = await TiredManhattanGenerator.GenerateBytes(backgroundStream, fontStream, text);
-        //var image = await TiredManhattanGenerator.Generate(stream, text);
+        IsWorking = true;
+        try
+        {
+            var text = TiredManhattanGenerator.Clean(TextToRender);
+            using var backgroundStream = await HttpClient.GetStreamAsync("assets/background.png");
+            using var fontStream = await HttpClient.GetStreamAsync("assets/KMKDSPK_.ttf");
+            ImageBytes = await TiredManhattanGenerator.GenerateBytes(backgroundStream, fontStream, text);
+            //var image = await TiredManhattanGenerator.Generate(stream, text);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+        finally
+        {
+            IsWorking = false;
+        }
     }
 }
