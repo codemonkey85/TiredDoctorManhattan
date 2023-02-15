ef44a7008ae3c0735023c14db836a9a9051001f7
Michael Bond <michael@bondcodes.com>
2023-02-15 02:11:35 -0500

    Use libman, update packages, update styling

--- TiredDoctorManhattan.Wasm/Pages/Index.razor.cs
+++ TiredDoctorManhattan.Wasm/Pages/Index.razor.cs
@@ -2,16 +2,17 @@ namespace TiredDoctorManhattan.Wasm.Pages;
 
 public partial class Index
 {
-    private string textToRender { get; set; } = default!;
-    private byte[]? imageBytes { get; set; }
-    private string? getImageBase64 => imageBytes is null ? null : Convert.ToBase64String(imageBytes);
+    private static string PageTitle => "Tired Doctor Manhattan";
+    private string TextToRender { get; set; } = default!;
+    private byte[]? ImageBytes { get; set; }
+    private string? GetImageBase64 => ImageBytes is null ? null : Convert.ToBase64String(ImageBytes);
 
     private async Task GenerateImage()
     {
-        var text = TiredManhattanGenerator.Clean(textToRender);
+        var text = TiredManhattanGenerator.Clean(TextToRender);
         using var backgroundStream = await HttpClient.GetStreamAsync("assets/background.png");
         using var fontStream = await HttpClient.GetStreamAsync("assets/KMKDSPK_.ttf");
-        imageBytes = await TiredManhattanGenerator.GenerateBytes(backgroundStream, fontStream, text);
+        ImageBytes = await TiredManhattanGenerator.GenerateBytes(backgroundStream, fontStream, text);
         //var image = await TiredManhattanGenerator.Generate(stream, text);
     }
 }
